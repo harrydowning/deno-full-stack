c0425a3b38dedf4cedd0a308322bd5a7c112a02f
harrydowning <harry.downing17@gmail.com>
2024-10-20 00:44:12 +0100

    add dev server

--- build.ts
+++ build.ts
@@ -1,14 +1,34 @@
+import fs from "node:fs";
 import esbuild from "esbuild";
 
-const result = await esbuild.build({
-  entryPoints: ["src/index.tsx"],
-  bundle: true,
-  platform: "browser",
-  outdir: "dist",
-  splitting: true,
-  format: "esm",
-  minify: true,
-  metafile: true,
+const outDir = "dist";
+const staticDir = "static";
+const watch = process.argv.includes("--watch");
+const port = 3000;
+
+fs.rmSync(outDir, { recursive: true, force: true });
+fs.mkdirSync(outDir);
+fs.readdirSync(staticDir).forEach((path) => {
+  fs.cpSync(`${staticDir}/${path}`, outDir);
 });
 
-console.log(esbuild.analyzeMetafileSync(result.metafile, { color: true }));
+const options = {
+  entryPoints: ["src/index.tsx"],
+  bundle: true,
+  platform: "browser" as const,
+  outdir: outDir,
+  splitting: true,
+  format: "esm" as const,
+  minify: true,
+  metafile: true as const,
+};
+
+if (watch) {
+  const ctx = await esbuild.context(options);
+  ctx.watch();
+  ctx.serve({ port, servedir: outDir });
+  console.log(`Serving at http://localhost:${port}`);
+} else {
+  const result = await esbuild.build(options);
+  console.log(esbuild.analyzeMetafileSync(result.metafile, { color: true }));
+}
