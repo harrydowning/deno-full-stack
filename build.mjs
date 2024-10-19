66e00c189af6175cc57a2f1bb6a5b5374babe15b
harrydowning <harry.downing17@gmail.com>
2024-10-19 17:35:36 +0100

    deno init

---
+++ build.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import esbuild from "esbuild";
+
+await esbuild.build({
+  entryPoints: ["src/index.tsx"],
+  bundle: true,
+  platform: "browser",
+  outfile: "dist/index.js",
+  minify: true,
+  metafile: true,
+});
